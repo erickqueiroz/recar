ac8be00ce81093e0707cf68261c9a3fae2058967
Erick Queiroz <erickqueiroz93@gmail.com>
2017-02-12 22:16:55 -0200

    First commit

---
+++ public/js/app.js
@@ -0,0 +1,9 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAwEW2jTcYyUU08rEjsRseW7TLY4vpqRnU",
+    authDomain: "loja-recar.firebaseapp.com",
+    databaseURL: "https://loja-recar.firebaseio.com",
+    storageBucket: "loja-recar.appspot.com",
+    messagingSenderId: "265884200470"
+  };
+  firebase.initializeApp(config);
